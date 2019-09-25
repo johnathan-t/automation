37799d9430bf91d905dc6e7f953968615ca84679
johnathan-t <55798304+johnathan-t@users.noreply.github.com>
2019-09-25 10:43:56 -0400

    Create client.ps1

---
+++ client.ps1
@@ -0,0 +1,80 @@
+# Automatically run windows updates from a scheduled task
+#
+# Revision 1.0
+#
+# Schedule this script from a task and make sure the server can access SERVER
+# over the network.
+#
+# Johnathan Tracz, 2019/04/26
+
+
+$lf = '\\server\users$\jtracz\scripts\serverLog.txt'
+$hn = (hostname) | Out-String
+$hn = $hn -replace '\s',''
+$dt = (Get-date) | Out-String
+
+foreach($line in Get-Content \\server\users$\jtracz\scripts\serv.txt) {
+    if($line -imatch 'install'){
+        "Installing..." >> $lf
+        "Current machine: $hn " >> $lf
+        "Time: $dt" >> $lf
+        cscript.exe "\\server\users$\jtracz\Scripts\WUA.vbs"
+    }
+    else 
+    {
+        $gd = Get-Date
+        "Check-in from $hn at $gd, nothing to install..." >> $lf
+    }
+}
+# SIG # Begin signature block
+# MIIJNgYJKoZIhvcNAQcCoIIJJzCCCSMCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
+# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQULVajSii5bzMs9//nGgL22CoK
+# iE+gggaBMIIGfTCCBWWgAwIBAgITNwAtmTVHaTIVRs++iAABAC2ZNTANBgkqhkiG
+# 9w0BAQsFADBnMRIwEAYKCZImiZPyLGQBGRYCY2ExEjAQBgoJkiaJk/IsZAEZFgJv
+# bjEYMBYGCgmSJomT8ixkARkWCHN0am9zaGFtMRIwEAYKCZImiZPyLGQBGRYCYWQx
+# DzANBgNVBAMTBlJPT1RDQTAeFw0xOTA0MTcxODUwMThaFw0yMDA0MTYxODUwMTha
+# MIGAMRIwEAYKCZImiZPyLGQBGRYCY2ExEjAQBgoJkiaJk/IsZAEZFgJvbjEYMBYG
+# CgmSJomT8ixkARkWCHN0am9zaGFtMRIwEAYKCZImiZPyLGQBGRYCYWQxDjAMBgNV
+# BAMTBVVzZXJzMRgwFgYDVQQDEw9Kb2huYXRoYW4gVHJhY3owggEiMA0GCSqGSIb3
+# DQEBAQUAA4IBDwAwggEKAoIBAQDzXyEY2zM42Ewmh8w8v8WKknVJGmCk60Km2HIa
+# Zd9H6wos2G2S62bg/ZV+w51PZfJoaGI0Zkr9/X7xCnLB2uGL3exU9OTYA8K0o4PN
+# gVIGyowm12mCPoYNmvl9IXQLVs4F3hVefILwG9wZ/u2QJMOWr4I50KrA3ubKnVTC
+# ZU+nxek8dEqEnXXdoPeFxjXq5BmXYYInSjU2U16I4tJ2fRXW4GKd2+fMGacTlIEl
+# RxDRVDymtDi9iA0x7D/OOYJJoiMXzk7b9UM6LsilWWSoTX3hVAdaP90yfamf3mDN
+# 1FfmQsmzgSUPX2dIvG1ZgjQRiD+vdlOPPKZQ3wHL827D6JnZAgMBAAGjggMGMIID
+# AjALBgNVHQ8EBAMCB4AwJQYJKwYBBAGCNxQCBBgeFgBDAG8AZABlAFMAaQBnAG4A
+# aQBuAGcwHQYDVR0OBBYEFCx1n8lN+GhiLbpgmdvav8JsuPaKMB8GA1UdIwQYMBaA
+# FK9SNk/0B5JkaIGXu3iW/b2jm3XkMIIBEwYDVR0fBIIBCjCCAQYwggECoIH/oIH8
+# hoG9bGRhcDovLy9DTj1ST09UQ0EoMSksQ049U0pILUNFUlQsQ049Q0RQLENOPVB1
+# YmxpYyUyMEtleSUyMFNlcnZpY2VzLENOPVNlcnZpY2VzLENOPUNvbmZpZ3VyYXRp
+# b24sREM9YWQsREM9c3Rqb3NoYW0sREM9b24sREM9Y2E/Y2VydGlmaWNhdGVSZXZv
+# Y2F0aW9uTGlzdD9iYXNlP29iamVjdENsYXNzPWNSTERpc3RyaWJ1dGlvblBvaW50
+# hjpodHRwOi8vU0pILUNFUlQuYWQuc3Rqb3NoYW0ub24uY2EvQ2VydEVucm9sbC9S
+# T09UQ0EoMSkuY3JsMIIBKQYIKwYBBQUHAQEEggEbMIIBFzCBsQYIKwYBBQUHMAKG
+# gaRsZGFwOi8vL0NOPVJPT1RDQSxDTj1BSUEsQ049UHVibGljJTIwS2V5JTIwU2Vy
+# dmljZXMsQ049U2VydmljZXMsQ049Q29uZmlndXJhdGlvbixEQz1hZCxEQz1zdGpv
+# c2hhbSxEQz1vbixEQz1jYT9jQUNlcnRpZmljYXRlP2Jhc2U/b2JqZWN0Q2xhc3M9
+# Y2VydGlmaWNhdGlvbkF1dGhvcml0eTBhBggrBgEFBQcwAoZVaHR0cDovL1NKSC1D
+# RVJULmFkLnN0am9zaGFtLm9uLmNhL0NlcnRFbnJvbGwvU0pILUNFUlQuYWQuc3Rq
+# b3NoYW0ub24uY2FfUk9PVENBKDEpLmNydDATBgNVHSUEDDAKBggrBgEFBQcDAzAz
+# BgNVHREELDAqoCgGCisGAQQBgjcUAgOgGgwYanRyYWN6QGFkLnN0am9zaGFtLm9u
+# LmNhMA0GCSqGSIb3DQEBCwUAA4IBAQAT8YJH/haGLRZ/QtnQRjKOTET6ljt78uyE
+# /e17Ja0i5kLFFCT/wLqmEZyBqNkmelkTyURyaNXgC2uyjtF0plS4dhXVDLwMFRyy
+# LV5WVmPHJ7I+AlllzAZrVpL/kEVQATQnRxnMOlctm/UryIRYlxMiNXGRbkh0zPde
+# VRwZ+tYCp7Y8maEAMpjG4d0IGqECHrCcMbczjUSLXukVenmzBt1pN2TCj+iVLRhu
+# jMTTBiaCL1GbMHzxirdaiVbaMWIPyVyisyq8hBdYyPRWrwaa4MAEubQdvVue8mJf
+# +bYuv20Ab3y23Vhv3oh7QYCAVdr274fP7knmCv2zF97OKwmj8lbMMYICHzCCAhsC
+# AQEwfjBnMRIwEAYKCZImiZPyLGQBGRYCY2ExEjAQBgoJkiaJk/IsZAEZFgJvbjEY
+# MBYGCgmSJomT8ixkARkWCHN0am9zaGFtMRIwEAYKCZImiZPyLGQBGRYCYWQxDzAN
+# BgNVBAMTBlJPT1RDQQITNwAtmTVHaTIVRs++iAABAC2ZNTAJBgUrDgMCGgUAoHgw
+# GAYKKwYBBAGCNwIBDDEKMAigAoAAoQKAADAZBgkqhkiG9w0BCQMxDAYKKwYBBAGC
+# NwIBBDAcBgorBgEEAYI3AgELMQ4wDAYKKwYBBAGCNwIBFTAjBgkqhkiG9w0BCQQx
+# FgQURah3oaGDdtkS1CImQVrbdS7AhSowDQYJKoZIhvcNAQEBBQAEggEAS08Fn85u
+# vo+h1gi1A0SCSQDolf/6f3dXYjG7AvursF31dXqPweNxnI03G5LLQhZWLl0fr+Fy
+# l2wITxTx8+9ObBtEpD3tVMuIYSRvqYnfpclWlZ9ttjgwCT3QrZW//Oj3PgPXLIfy
+# ksEiDe75K9DYFwld7WbCuNoBw1fc4gGNUarTnnXAw5qM9AF/m7/4bex3ai3Coq7Y
+# 0+3BFZK2uGvci9ElYTgue8/c41izVyRNxuY6kkIgCIqmxZSylLTID2heivK728sJ
+# RoPBkuT37DxCPNV96GGNd4zTW0TitlF3pXFkJY08JY2yqdZOhTbdoi8zeeAILpXm
+# GT/ATiZSS8h0pw==
+# SIG # End signature block
